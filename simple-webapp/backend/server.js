2a60666629797773a3cd7cdc6af832cd0cb1be4d
student <student@localhost.localdomain>
2023-09-22 21:31:57 +0400

    Refactor1

--- simple-webapp/backend/server.js
+++ simple-webapp/backend/server.js
@@ -4,12 +4,12 @@ const greet = require("./greet");
 
 const server = http.createServer((req, res) => {
     const { name } = url.parse(req.url, true).query;
-    const greeting = greet(name);
+    const greetng = greet(name);
 
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.end(greeting);
+    res.end(greetng);
 });
 
 const port = 3000;
